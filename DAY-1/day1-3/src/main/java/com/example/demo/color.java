5cbc130699c69cad1a334cbd11d35b52f96f8342
MOHAMED-HASHEEM-N <727721euit092@skcet.ac.in>
2023-02-22 21:28:54 +0530

    First commit

---
+++ DAY-1/day1-3/src/main/java/com/example/demo/color.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class color {
+	String str1="Blue";
+	 @GetMapping("/game")
+	 public String getMyFav()
+	 {
+	  return "My favourite color is "+str1;
+	 }
+}
